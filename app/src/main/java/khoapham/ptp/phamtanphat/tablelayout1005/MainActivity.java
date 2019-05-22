505ac377da41606f4bf68edfa479393407136315
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-22 21:05:59 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/tablelayout1005/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/tablelayout1005/MainActivity.java
@@ -2,12 +2,20 @@ package khoapham.ptp.phamtanphat.tablelayout1005;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    String b = "Toan cuc";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    private void Hello(){
+        String a ="Cuc bo";
     }
 }

89a30957e80eca0b92839479564d795e1007a0ad
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-24 19:55:30 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/tablelayout1005/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/tablelayout1005/MainActivity.java
@@ -1,21 +1,25 @@
 package khoapham.ptp.phamtanphat.tablelayout1005;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TableLayout;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    String b = "Toan cuc";
+    //1 : Khai bao view can tuong tac
+    TableLayout tblBackground;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //anh xa
+        tblBackground = findViewById(R.id.tablelayout);
+        //viet code
+        tblBackground.setBackgroundColor(Color.BLUE);
+    }
 
-    }
-    private void Hello(){
-        String a ="Cuc bo";
-    }
 }
